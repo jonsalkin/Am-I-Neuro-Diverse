2b13b2d3fdb3a66bf61253b50f241a00238cffa8
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-06-15 14:53:45 -0400

    Initial Commit

---
+++ Am I Neuro-Diverse/Am_I_Neuro_DiverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Am_I_Neuro_DiverseApp.swift
+//  Am I Neuro-Diverse
+//
+//  Created by Jon Salkin on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Am_I_Neuro_DiverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
